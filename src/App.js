0d337e2a37fdd4268bd5564b3543477b6a966f90
Bektemir1 <kudajberdievbektemir@gmail.com>
2022-04-13 23:45:36 +0600

    broke home page into small components and style menu item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './App.css';
-import HomePage from './home-page'
+import HomePage from './pages/homepage/home-page'
 function App() {
   return (
     <div>
---
+++ src/pages/homepage/home-page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../components/directory/directory';
+import './home-page.scss'
+ const HomePage = () =>{
+   return(
+        <div className='homepage'>
+            <Directory/>
+        </div>
+   )
+}
+
+export default HomePage;
